699d66c96a6f2436cb7326c3c9384896efc15577
JGries <jacksongriessen@gmail.com>
2020-06-30 13:01:47 -0500

    added search.js

---
+++ search.js
@@ -0,0 +1,43 @@
+//call API when clicked or submited with the right parameters
+
+
+
+//store response in local storage or variable
+
+var inputEl = $("input")
+var drinks;
+var beers;
+
+
+    for (var i = 0; i < drinks.length; i++) {
+    var drink = drinks[i]
+    if (imput == drink) {
+        //display infromation and images
+    }
+    for (var i = 0; i < beers.length; i++) {
+        var beer = beers[i]
+        if (imput == beer) {
+           //display infromation and images
+        }
+
+inputEl.on("click", function () {
+    console.log(this)
+})
+
+inputEl.keypress(function (event) {
+    if (event.originalEvent.keyCode === 13) {
+        var value = $(this).val();
+        var key = $(this).attr("class")
+        console.log(value)
+        console.log(key)
+        localStorage.setItem(key, value)
+    }
+
+})
+
+//Create result cards
+    //card body
+    //drink title w/ link to detailed results
+    //pictures
+//create detailed results
+    //take account of all info were getting and displaying it properly
