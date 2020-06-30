89a7bc69c6474bc0305867fdc8681af843eab07f
JGries <jacksongriessen@gmail.com>
2020-06-30 13:58:37 -0500

    wanting to pull master

--- scrypt.js
+++ scrypt.js
@@ -1,6 +1,39 @@
 //call API when clicked or submited with the right parameters
 
+
+
 //store response in local storage or variable
+
+var inputEl = $("input")
+var drinks;
+var beers;
+
+
+    for (var i = 0; i < drinks.length; i++) {
+    var drink = drinks[i]
+    if (imput == drink) {
+        //do stuff
+    }
+    for (var i = 0; i < beers.length; i++) {
+        var beer = beers[i]
+        if (imput == beer) {
+            //do stuff
+        }
+
+inputEl.on("click", function () {
+    console.log(this)
+})
+
+inputEl.keypress(function (event) {
+    if (event.originalEvent.keyCode === 13) {
+        var value = $(this).val();
+        var key = $(this).attr("class")
+        console.log(value)
+        console.log(key)
+        localStorage.setItem(key, value)
+    }
+
+})
 
 //Create result cards
     //card body
